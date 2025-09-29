b0c7d9443fa30f893c8cc29d785bcf6166b554c2
yeruvabala <reddyakshay243@gmail.com>
2025-09-29 16:28:25 -0400

    Create types.ts

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+export type ParsedFields = {
+  date?: string | null;
+  stakes?: string | null;
+  position?: string | null;
+  cards?: string | null;
+  villain_action?: string | null;
+  gto_strategy?: string | null;
+  exploit_deviation?: string | null;
+  learning_tag?: string[];
+};
