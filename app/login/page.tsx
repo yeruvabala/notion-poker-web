5749b02961a9d277acd07d2b308fcacc638eb18a
yeruvabala <reddyakshay243@gmail.com>
2025-10-03 08:32:20 -0400

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,6 @@
-// app/login/page.tsx
 import { Suspense } from 'react';
 import LoginClient from './LoginClient';
 
-// Avoid static export/prerender for this page
 export const dynamic = 'force-dynamic';
 
 export default function Page() {
