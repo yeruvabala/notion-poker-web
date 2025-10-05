a9553df44f48a1468d0df5529db39681e04d7d4c
yeruvabala <reddyakshay243@gmail.com>
2025-10-04 21:31:05 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,3 +7,5 @@ module.exports = {
   theme: { extend: {} },
   plugins: [],
 };
+
+export default config;
