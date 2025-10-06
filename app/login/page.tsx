f21f186358e2116dbbfe7cb384ba7083a3cee2e4
yeruvabala <reddyakshay243@gmail.com>
2025-10-06 00:07:16 -0400

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,7 @@
-// app/login/page.tsx
 import { Suspense } from 'react';
 import LoginClient from './LoginClient';
 
-// important to avoid the “missing suspense” warning in prod
+// avoid the “missing suspense” warning in prod
 export const dynamic = 'force-dynamic';
 
 export default function Page() {
